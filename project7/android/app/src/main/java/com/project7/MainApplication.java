2a46f9e2132d19d4012fc3eac34a799a89790380
heogong <heogong@gmail.com>
2018-10-29 21:33:57 +0900

    project7 추가

---
+++ project7/android/app/src/main/java/com/project7/MainApplication.java
@@ -0,0 +1,78 @@
+package com.project7;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.teamsf.daummap.DaumMapPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import java.security.MessageDigest;
+import android.util.Log;
+import java.security.NoSuchAlgorithmException;
+
+import static com.kakao.util.helper.Utility.getPackageInfo;
+
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+          new DaumMapPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+    Log.i("Hash:",getKeyHash(this));
+  }
+
+  public String getKeyHash(final Context context) {
+    PackageInfo packageInfo = getPackageInfo(context, PackageManager.GET_SIGNATURES);
+    if (packageInfo == null) {
+      return null;
+    }
+
+    for (Signature signature : packageInfo.signatures) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA");
+            md.update(signature.toByteArray());
+            return android.util.Base64.encodeToString(md.digest(), android.util.Base64.NO_WRAP);
+        } catch (NoSuchAlgorithmException e) {
+            Log.w("HashKey", "Unable to get MessageDigest. signature=" + signature, e);
+        }
+    }
+    return null;
+  }
+
+}
