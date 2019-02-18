a326ebe8411e50ba7169e9f4da52dd921de6e387
heogong <heogong@gmail.com>
2019-02-18 21:24:21 +0900

    와꾸 작업 중

--- project19/InvaildId.js
+++ project19/InvaildId.js
@@ -34,7 +34,9 @@ class InvaildId extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      checkBox : false
+      checkBox : false,
+      findId : false,
+      tabIndex : 0
     };
   }
 
@@ -52,65 +54,175 @@ class InvaildId extends Component {
           </Body>
           <Right style={{flex:1}}></Right>
         </Header>
-        <View style={{flex:1}}>
+
+        <View style={[styles.mg10, {flex:1}]}>
 
           <View style={{flex:1}}>
             <H1>본인 인증하고</H1>
-            <H1>아이디를 찾으세요</H1>
+            { (this.state.tabIndex == 0 ) ? (
+              <H1>아이디를 찾으세요</H1>
+            ) : (
+              <H1>비밀번호를 찾으세요</H1>
+            )}
           </View>
 
           <View style={{flex:3}}>
-            <Tabs>
+            <Tabs
+              onChangeTab={(obj, ref) => this.setState({tabIndex : obj.i}) }
+            >
               <Tab 
                 heading="아이디찾기" 
                 activeTabStyle={{backgroundColor:'#28c8f5'}} 
                 tabStyle={{ backgroundColor:'#d6f1ff'}}
                 textStyle={{color:'#28c8f5'}}
                 activeTextStyle={{color:'#fff'}}
-                >
-                <View style={{backgroundColor : '#28c8f5'}}>
-                  <Item regular style={[styles.mb15, {backgroundColor:'#fff', borderColor : '#fff', height : 50}]}>
-                    <Input placeholder="이름" />
+              >
+              { (!this.state.findId) ? (
+                <View style={[styles.pd20, {flex :1, paddingTop : 35, backgroundColor : '#28c8f5'}] }>
+                  <Item regular style={[styles.mb15, {backgroundColor:'#fff', borderColor : '#fff', height : inputHeight}]}>
+                    <Input 
+                      style={{fontSize : 12}}
+                      placeholder="이름" 
+                    />
                   </Item>
-                  <Item regular style={[styles.mb15, {backgroundColor:'#fff', borderColor : '#fff', height : 50}]}>
-                    <Input placeholder="핸드폰번호(하이픈 -제외하고 입력)" />
+                  <Item regular style={[styles.mb15, {backgroundColor:'#fff', borderColor : '#fff', height : inputHeight}]}>
+                    <Input 
+                      style={{fontSize : 12}}
+                      placeholder="핸드폰번호(하이픈 -제외하고 입력)" 
+                    />
                   </Item>
 
                   <View style={{flexDirection : 'row'}}>
-                    <View style={{width:'65%'}}>
-                      <Item regular style={[styles.mb15, {backgroundColor:'#fff', borderColor : '#fff', height : 50}]}>
-                        <Input placeholder="인증번호입력" />
+                    <View style={{width : '60%', paddingRight : 10}}>
+                      <Item regular style={{backgroundColor:'#fff', borderColor : '#fff', height : inputHeight}}>
+                        <Input 
+                          style={{fontSize : 12}}
+                          placeholder="인증번호입력" />
                       </Item>
                     </View>
-                    <View style={{width:'35%'}}>
-                      <Button block info style={{
-                        marginRight : 5,
-                        borderColor : '#fff',
-                        borderTopWidth : 1,
-                        borderBottomWidth : 1,
-                        borderLeftWidth : 1,
-                        borderRightWidth : 1
+                    <View style={{width: '40%'}}>
+                      <Button block info 
+                        onPress={ () => alert("인증번호전송")}
+                        style={{
+                          height : inputHeight,
+                          borderColor : '#fff',
+                          borderTopWidth : 1,
+                          borderBottomWidth : 1,
+                          borderLeftWidth : 1,
+                          borderRightWidth : 1, 
+                          elevation:0
                         }} >
-                        <Text>인증번호전송</Text>
+                        <Text style={{fontSize : 12}}>인증번호전송</Text>
                       </Button>
                     </View>
+                  </View>
 
+                  <View>
+                    <Text style={styles.whiteFont}>유효한 인증번호입니다.</Text>
                   </View>
                 </View>
+
+              ) : (
+                <View style={{flex : 1, backgroundColor : '#28c8f5', justifyContent : 'center', alignItems : 'center'}}>
+                  <Text style={styles.whiteFont}>박정진님은 가입되어 있으며</Text>
+                  <Text style={styles.whiteFont}>회원님의 아이디는 어쩌구젖구</Text>
+                  <Text style={styles.whiteFont}>ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ</Text>
+                </View>
+              )}
+                
               </Tab>
+
               <Tab heading="비밀번호찾기" 
                 activeTabStyle={{backgroundColor:'#28c8f5'}} 
                 tabStyle={{ backgroundColor:'#d6f1ff'}}
                 textStyle={{color:'#28c8f5'}}
                 activeTextStyle={{color:'#fff'}}>
-              <Text>bbbbbbbbbbbbbbbbbbbbbbbb</Text>
+                <View style={[styles.pd20, {flex :1, paddingTop : 35, backgroundColor : '#28c8f5'}] }>
+                  <Item regular style={[styles.mb15, {backgroundColor:'#fff', borderColor : '#fff', height : inputHeight}]}>
+                    <Input 
+                      style={{fontSize : 12}}
+                      placeholder="이메일 아이디" 
+                    />
+                  </Item>
+                  <Item regular style={[styles.mb15, {backgroundColor:'#fff', borderColor : '#fff', height : inputHeight}]}>
+                    <Input 
+                      style={{fontSize : 12}}
+                      placeholder="이름" 
+                    />
+                  </Item>
+                  <Item regular style={[styles.mb15, {backgroundColor:'#fff', borderColor : '#fff', height : inputHeight}]}>
+                    <Input 
+                      style={{fontSize : 12}}
+                      placeholder="핸드폰번호(하이픈 -제외하고 입력)" 
+                    />
+                  </Item>
+
+                  <View style={{flexDirection : 'row'}}>
+                    <View style={{width : '60%', paddingRight : 10}}>
+                      <Item regular style={{backgroundColor:'#fff', borderColor : '#fff', height : inputHeight}}>
+                        <Input 
+                          style={{fontSize : 12}}
+                          placeholder="인증번호입력" />
+                      </Item>
+                    </View>
+                    <View style={{width: '40%'}}>
+                      <Button block info 
+                        onPress={ () => alert("인증번호전송")}
+                        style={{
+                          height : inputHeight,
+                          borderColor : '#fff',
+                          borderTopWidth : 1,
+                          borderBottomWidth : 1,
+                          borderLeftWidth : 1,
+                          borderRightWidth : 1, 
+                          elevation:0
+                        }} >
+                        <Text style={{fontSize : 12}}>인증번호전송</Text>
+                      </Button>
+                    </View>
+                  </View>
+
+                  <View>
+                    <Text style={styles.whiteFont}>유효한 인증번호입니다.</Text>
+                  </View>
+
+                </View>
               </Tab>
             </Tabs>
           </View>
 
-          <View style={{flex:1}}>
-          </View>
-          
+          <View style={{flex:1, justifyContent : 'center'}}>
+
+          { 
+            (this.state.tabIndex == 0) ? (
+              (!this.state.findId) ? (
+                <Button block info bordered 
+                  onPress={ () => this.setState({findId : true})}
+                  style={{
+                    elevation:0
+                  }} >
+                  <Text>확인</Text>
+                </Button>
+              ) : (
+                <Button block info bordered 
+                  onPress={ () => this.setState({findId : false})}
+                  style={{
+                    elevation:0
+                  }} >
+                  <Text>로그인하기</Text>
+                </Button>
+              )
+            ) : (
+              <Button block info bordered 
+                onPress={ () => alert("비밀번호 확인")}
+                style={{
+                  elevation:0
+                }} >
+                <Text>비밀번호 확인</Text>
+              </Button>
+            )
+          }
+           </View>
         </View>
 
       </Container>
@@ -118,15 +230,10 @@ class InvaildId extends Component {
   }
 }
 
-const layoutCount = 4; // 화면 분할 개수 사이즈
-const snsDivideCount = 12;
-
+const layoutCount = 5; // 화면 분할 개수 사이즈
 const viewportHeight = Dimensions.get('window').height;
-const viewportWidth = Dimensions.get('window').width;
 
-const logoHeight = (viewportHeight / layoutCount);
-const snsIconSize = (viewportWidth / snsDivideCount);
-
+const inputHeight = (viewportHeight / layoutCount) * 0.3;
 
 const styles = StyleSheet.create({
   container: {
@@ -157,21 +264,26 @@ const styles = StyleSheet.create({
     marginLeft : 10,
     marginRight : 10
   },
+  pd10 : {
+    paddingTop : 10,
+    paddingBottom : 10,
+    paddingLeft : 10,
+    paddingRight : 10
+  },
+  pd20 : {
+    paddingTop : 20,
+    paddingBottom : 20,
+    paddingLeft : 20,
+    paddingRight : 20
+  },
   greyFont : {
     color : '#BDBDBD',
     fontSize : 15
   },
-  snsIcon : {
-    justifyContent : 'center',
-    alignItems : 'center',
-    borderColor : '#fff',
-    borderBottomWidth : 1, 
-    borderTopWidth : 1,
-    borderLeftWidth : 1,
-    borderRightWidth : 1,
-    height : snsIconSize + 20, width : snsIconSize + 20,
-    borderRadius : 5
-  }
+  whiteFont : {
+    color : '#FFF',
+    fontSize : 15
+  },
 });
 
 export default InvaildId;
