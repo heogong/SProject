77adbc087551eb269fdb591af32e52e5135cfef1
Jung Hyun Yoo <jjhyunyy@gmail.com>
2019-03-30 15:29:27 +0900

    123123

--- project12/src/FirstScreen/Pages/Join/Partner/AgreeTermsService.js
+++ project12/src/FirstScreen/Pages/Join/Partner/AgreeTermsService.js
@@ -114,58 +114,55 @@ class AgreeTermsService extends Component {
             
           </View>
 
-          <View style={[styles.alignItemsCenter, {paddingTop: 40, paddingBottom: 40, borderBottomWidth: 1, borderBottomColor: "#c9cacb"}]}>
+          <View style={[styles.alignItemsCenter, {paddingTop: 30, paddingBottom: 30, borderBottomWidth: 1, borderBottomColor: "#c9cacb"}]}>
             <Text style={styles.greyFont}>쿨리닉 내의 원활한 서비스 이용을 위해서는</Text>
             <Text style={styles.greyFont}>아래의 필수 항목에 대한 동의가 필요합니다</Text>
           </View>
 
           <View style={localStyles.termsWrap}>
-            <View style={[styles.fx3, styles.alignItemsStart, styles.justiConBetween]}>
-              <Text style={[styles.blueFont, styles.mb20]}>서비스 이용약관 동의(필수)</Text>
-              <Text style={[styles.blueFont, styles.mb20]}>위치기반 서비스 이용약관 동의(필수)</Text>
-              <Text style={[styles.blueFont, styles.mb20]}>개인정보 수집 동의(필수)</Text>
-              <Text style={[styles.blueFont, styles.mb20]}>개인정보 제 3자 제공 동의서(필수)</Text>
-              <Text style={[styles.blueFont, styles.mb20]}>마케팅 전체 수신동의(선택)</Text>
-            </View>
-              
-            <View style={[styles.fx1, styles.fxDirRow]}>
-              
-              <View style={[styles.fx1, styles.alignItemsEnd, styles.justiConBetween]}>
-              
-                <View style={[styles.fxDirRow, styles.fx1]}>
-                  <CheckBox checked={this.state.checkBox1}
-                    onPress={async () => { await this.setState({checkBox1 : !this.state.checkBox1}), await this.toggleSwitch()}}
-                    style={[styles.checkboxReset, {borderColor: color.defaultColor}]}
-                  />
-                </View>
-
-                <View style={[styles.fxDirRow, styles.fx1]}>
-                  <CheckBox checked={this.state.checkBox2}
-                    onPress={async () => { await this.setState({checkBox2 : !this.state.checkBox2}), await this.toggleSwitch() }}
-                    style={[styles.checkboxReset, {borderColor: color.defaultColor}]}
-                  />
-                </View>
-
-                <View style={[styles.fxDirRow, styles.fx1]}>
-                  <CheckBox checked={this.state.checkBox3}
-                    onPress={async () => { await this.setState({checkBox3 : !this.state.checkBox3}), await this.toggleSwitch() }}
-                    style={[styles.checkboxReset, {borderColor: color.defaultColor}]}
-                  />
-                </View>
-                <View style={[styles.fxDirRow, styles.fx1]}>
-                  <CheckBox checked={this.state.checkBox4}
-                    onPress={async () => { await this.setState({checkBox4 : !this.state.checkBox4}), await this.toggleSwitch() }}
-                    style={[styles.checkboxReset, {borderColor: color.defaultColor}]}
-                  />
-                </View>
-                <View style={[styles.fxDirRow, styles.fx1]}>
-                  <CheckBox checked={this.state.checkBox5}
-                    onPress={() => { this.setState({checkBox5 : !this.state.checkBox5}) }}
-                    style={[styles.checkboxReset, {borderColor: color.defaultColor}]}
-                  />
-                </View>
+            <View style={styles.fx5}>
+              <View style={[styles.fxDirRow, styles.justiConBetween]}>
+                <Text style={[styles.blueFont, styles.mb20]}>서비스 이용약관 동의(필수)</Text>
+                <CheckBox 
+                  checked={this.state.checkBox1}
+                  onPress={() => { this.setState({checkBox1 : !this.state.checkBox1}) }}
+                  style={[styles.checkboxReset, {borderColor: color.defaultColor}]}
+                />
+              </View>
+              <View style={[styles.fxDirRow, styles.justiConBetween]}>
+                <Text style={[styles.blueFont, styles.mb20]}>위치기반 서비스 이용약관 동의(필수)</Text>
+                <CheckBox 
+                  checked={this.state.checkBox2}
+                  onPress={() => { this.setState({checkBox2 : !this.state.checkBox2}) }}
+                  style={[styles.checkboxReset, {borderColor: color.defaultColor}]}
+                />
+              </View>
+              <View style={[styles.fxDirRow, styles.justiConBetween]}>
+                <Text style={[styles.blueFont, styles.mb20]}>개인정보 수집 동의(필수)</Text>
+                <CheckBox 
+                  checked={this.state.checkBox3}
+                  onPress={() => { this.setState({checkBox3 : !this.state.checkBox3}) }}
+                  style={[styles.checkboxReset, {borderColor: color.defaultColor}]}
+                />
+              </View>
+              <View style={[styles.fxDirRow, styles.justiConBetween]}>
+                <Text style={[styles.blueFont, styles.mb20]}>개인정보 제 3자 제공 동의서(필수)</Text>
+                <CheckBox 
+                  checked={this.state.checkBox4}
+                  onPress={() => { this.setState({checkBox4 : !this.state.checkBox4}) }}
+                  style={[styles.checkboxReset, {borderColor: color.defaultColor}]}
+                />
+              </View>
+              <View style={[styles.fxDirRow, styles.justiConBetween]}>
+                <Text style={[styles.blueFont, styles.mb20]}>마케팅 전체 수신동의(선택)</Text>
+                <CheckBox 
+                  checked={this.state.checkBox5}
+                  onPress={() => { this.setState({checkBox5 : !this.state.checkBox5}) }}
+                  style={[styles.checkboxReset, {borderColor: color.defaultColor}]}
+                />
               </View>
             </View>
+
           </View>
 
           <View style={styles.footerBtnWrap}>
@@ -195,7 +192,8 @@ class AgreeTermsService extends Component {
 const localStyles = StyleSheet.create({
   termsWrap: {
     marginTop: 27,
-    flexDirection : "row"
+    flexDirection : "row",
+    flex: 1
   }
 });
 
